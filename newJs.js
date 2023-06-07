0e20927f3702fdc89a63a35226a619b3414c7a4d
Panchanand <panchanand.jha.-nd@disney.com>
2023-06-07 20:54:01 +0530

    added new htmls and descriptions , pdf

--- newJs.js
+++ newJs.js
@@ -1,45 +1,54 @@
-gsap.registerPlugin(ScrollTrigger);
-// REVEAL //
-gsap.utils.toArray(".revealUp").forEach(function (elem) {
-  ScrollTrigger.create({
-    trigger: elem,
-    start: "top 80%",
-    end: "bottom 20%",
-    markers: true,
-    onEnter: function () {
-      gsap.fromTo(
-        elem,
-        { y: 100, autoAlpha: 0 },
-        {
-          duration: 1.25,
-          y: 0,
-          autoAlpha: 1,
-          ease: "back",
-          overwrite: "auto"
-        }
-      );
-    },
-    onLeave: function () {
-      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-    },
-    onEnterBack: function () {
-      gsap.fromTo(
-        elem,
-        { y: -100, autoAlpha: 0 },
-        {
-          duration: 1.25,
-          y: 0,
-          autoAlpha: 1,
-          ease: "back",
-          overwrite: "auto"
-        }
-      );
-    },
-    onLeaveBack: function () {
-      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-    }
-  });
-});
+// const box = document.querySelector('.twist');
+// const slider = document.querySelector('input[type="range"]');
+// slider.addEventListener('click', () => {
+//   for (let i=0; i<360; i++){
+//   box.setAttribute('style', `transform: rotate3d(1, 1, 1, ${Math.floor(Math.random() * 10)}deg)`);
+// }
+// });
+
+
+// gsap.registerPlugin(ScrollTrigger);
+// // REVEAL //
+// gsap.utils.toArray(".revealUp").forEach(function (elem) {
+//   ScrollTrigger.create({
+//     trigger: elem,
+//     start: "top 80%",
+//     end: "bottom 20%",
+//     markers: true,
+//     onEnter: function () {
+//       gsap.fromTo(
+//         elem,
+//         { y: 100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: "back",
+//           overwrite: "auto"
+//         }
+//       );
+//     },
+//     onLeave: function () {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+//     },
+//     onEnterBack: function () {
+//       gsap.fromTo(
+//         elem,
+//         { y: -100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: "back",
+//           overwrite: "auto"
+//         }
+//       );
+//     },
+//     onLeaveBack: function () {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+//     }
+//   });
+// });
 
 
 window.addEventListener('mouseup', (e) => {
@@ -56,3 +65,6 @@ window.addEventListener('mouseup', (e) => {
     // element we actually clicked on
     e.target.style.fill = fill
   })
+
+
+
